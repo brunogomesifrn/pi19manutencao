dabcf69ba66580734e2bdbe3e394ef46f4bcfe27
Mirlayne <mirlayne.lopes@gmail.com>
2019-10-25 15:50:01 -0300

    ajeitando css das paginas de problemas, mas ainda falta muita coisa. s

--- pi19manutencao/settings.py
+++ pi19manutencao/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'widget_tweaks',
     'core',
+    'livereload',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'pi19manutencao.urls'
