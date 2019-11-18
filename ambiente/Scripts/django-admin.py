f03c7146a08d514d41946e710e538f742fcf19c8
mirlayne <mirlayne.lopes@gmail.com>
2019-11-18 10:34:11 -0300

    retorna normal

--- ambiente/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\20161194010002\downloads\pi19manutencao\ambiente\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
