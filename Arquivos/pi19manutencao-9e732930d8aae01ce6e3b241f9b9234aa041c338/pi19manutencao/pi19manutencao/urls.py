1d1138edd825bc3764d1234c26413164821af0f1
mirlayne <mirlayne.lopes@gmail.com>
2019-11-21 16:45:59 -0300

    bruna botava esperanca

---
+++ Arquivos/pi19manutencao-9e732930d8aae01ce6e3b241f9b9234aa041c338/pi19manutencao/pi19manutencao/urls.py
@@ -0,0 +1,41 @@
+"""pi19manutencao URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core.views import index, perfil, cadastro, dados, pergunta_listar, cadastrar_pergunta, editar, apagar, problema1, problema2, problema3, problema4, problema5, problema6
+from django.contrib.auth import views as auth_views
+from core.views import paginatexte
+urlpatterns = [
+
+    path('paginatexte/', paginatexte, name='paginatexte'),
+    path('problema1/', problema1, name='problema1'),
+    path('problema2/', problema2, name='problema2'),
+    path('problema3/', problema3, name='problema3'),
+    path('problema4/', problema4, name='problema4'),
+    path('problema5/', problema5, name='problema5'),
+    path('problema6/', problema6, name='problema6'),
+    path('apagar/<int:id>', apagar, name='apagar'),
+    path('editar/<int:id>', editar, name='editar'),
+    path('cadastrar_pergunta', cadastrar_pergunta, name='cadastrar_pergunta'),
+    path('pergunta', pergunta_listar, name='pergunta'),
+	path('', index, name='index'),
+	path('perfil/', perfil, name='perfil'),
+	path('cadastro/', cadastro, name='cadastro'),
+	path('dados/<int:id>/', dados, name='dados'),
+	path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+]
